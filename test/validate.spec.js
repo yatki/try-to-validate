7d7e0df0ec1343cc72a1c2331b980858bdb5e7c2
mehmet <mehmet.yatki@heavenhr.de>
2017-08-30 16:28:25 +0200

    test syntax update

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,31 +1,31 @@
 import test from 'ava';
 import tryToValidate from '../index';
 
-test('if properties exist, should return value', async t => {
+test('if properties exist, should return value', t => {
   const me = {x: {y: {z: "I'm here"}}};
 
   t.is(tryToValidate(() => me.x.y.z), "I'm here");
 });
 
-test('if condition match, should return true', async t => {
+test('if condition match, should return true', t => {
   const me = {x: {y: {z: "I'm here"}}};
 
   t.is(tryToValidate(() => me.x.y.z === "I'm here"), true);
 });
 
-test('if condition match, should return bool', async t => {
+test('if condition match, should return bool', t => {
   const me = {x: {y: {z: "I'm here"}}};
 
   t.not(tryToValidate(() => me.x.y.z === "I'm here"), "I'm here");
 });
 
-test("if properties doesn't exist, should return false", async t => {
+test("if properties doesn't exist, should return false", t => {
   const me = {x: {y: {z: "I'm here"}}};
 
   t.is(tryToValidate(() => me.a.b.c), false);
 });
 
-test("if properties doesn't exist, should not return true", async t => {
+test("if properties doesn't exist, should not return true", t => {
   const me = {x: {y: {z: "I'm here"}}};
 
   t.not(tryToValidate(() => me.a.b.c), true);
